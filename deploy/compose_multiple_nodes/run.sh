b7c51b8cce5b7500cc984064db051ad89457e1d8
IlyaDolinskiy <dolinsky94@yandex.ru>
2022-12-04 00:12:02 +0300

    Add compose scale

---
+++ deploy/compose_multiple_nodes/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../../project/back
+mvn clean install
+cd ../../deploy/compose
+
+cd ../../project/web
+mvn clean install
+cd ../../deploy/compose
+
+docker compose build
+
+docker compose up --scale my-web=5
